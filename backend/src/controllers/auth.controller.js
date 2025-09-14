b51d38709483183d8cdd58b7cd000579a4856d22
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-09-14 12:21:13 +0530

    feat: restructure backend; add auth route/controller and server entry; update package scripts

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (_, res) => {
+    res.send("this signUp controller")
+}
+export const login = (_, res) => {
+    res.send("this login controller")
+}
+export const logout = (_, res) => {
+    res.send("this logout controller")
+}
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from "express"
+import "dotenv/config"
+import authRoutes from "./routes/auth.route.js"
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use("/api/auth",authRoutes)
+
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`)
+})
