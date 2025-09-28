8270505c569440a2f7e6b838e47486fc43d496f8
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-09-28 23:45:41 +0530

    feat: implement routing and add pages for Home, Login, Signup, Notification, Onboarding, Call, and Chat

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SingupPage from "./pages/SingupPage.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+
 function App() {
     return (
-        <div className="min-h-screen flex items-center justify-center bg-base-200" data-theme="dark --prefersdark"> 
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="card-title">Welcome to Video Calling App</h2>
-                    <p className="text-base-content">
-                        This is a starter template using <span className="font-bold">DaisyUI</span>!
-                    </p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Get Started</button>
-                    </div>
-                </div>
-            </div>
+        <div className="min-h-screen flex items-center justify-center bg-base-200" data-theme="dark --prefersdark">
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SingupPage />} />
+                <Route path="/notification" element={<NotificationPage />} />
+                <Route path="/onboard" element={<OnboardingPage />} />
+                <Route path="/call" element={<CallPage />} />
+                <Route path="/chat" element={<ChatPage />} />
+            </Routes>
         </div>
     );
 }
