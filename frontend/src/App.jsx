2cb8c5a781ac07311f04d6d6aaba3799bcaca27c
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-09-29 00:26:52 +0530

    feat: add axios instance and integrate react-query for data fetching

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,34 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
-import SingupPage from "./pages/SingupPage.jsx";
+import SignupPage from "./pages/SingupPage.jsx";
 import NotificationPage from "./pages/NotificationPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
+import toast, { Toaster } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query";
 
-function App() {
+const App = () => {
+    const {data} = useQuery({
+        queryKey:"todos",
+        queryFn:async()=>{
+            
+        }
+    })
     return (
-        <div className="min-h-screen flex items-center justify-center bg-base-200" data-theme="dark --prefersdark">
+        <div className="">
+            <button className="m-5 outline-1 p-2 bg-amber-400" onClick={() => { toast.success("hello toasty") }}>Show Toast</button>
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/signup" element={<SingupPage />} />
+                <Route path="/signup" element={<SignupPage />} />
                 <Route path="/notification" element={<NotificationPage />} />
                 <Route path="/onboard" element={<OnboardingPage />} />
                 <Route path="/call" element={<CallPage />} />
                 <Route path="/chat" element={<ChatPage />} />
             </Routes>
+            <Toaster />
         </div>
     );
 }
