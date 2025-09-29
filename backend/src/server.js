2377c7fcec0c0050d280227297f1dc88bd3599ae
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-09-29 13:03:02 +0530

    feat: add CORS middleware to enable cross-origin requests from the frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
+import cors from "cors"
 import { connectDB } from "./config/db.js"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/users.route.js"
@@ -11,12 +12,18 @@ import chatRoutes from "./routes/chat.route.js"
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}))
+
 app.use(express.json())
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
 
 
 connectDB()
