ae6845e799bede9f9e3761de1de39c0d2f2297bf
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-17 14:13:22 +0530

    feat: Refactor server setup for production, enhance axios configuration, and remove obsolete scripts

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,12 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import "dotenv/config"
 import cors from "cors"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 import { connectDB } from "./config/db.js"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/users.route.js"
@@ -13,9 +19,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "http://localhost:5173",
   credentials: true
 }))
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -23,6 +30,14 @@ app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  const frontendDistPath = path.join(__dirname, "../../frontend/dist")
+  app.use(express.static(frontendDistPath))
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"))
+  })
+}
 
 
 
